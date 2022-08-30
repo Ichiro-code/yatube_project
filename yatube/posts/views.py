87637848d598672917990f236d1f9769b27576f7
Константин Прудников <prudnikov1992@list.ru>
2022-08-30 15:10:22 +0300

    Ver 1.5

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
-def index (request):
-    return HttpResponse('Главная страница')
+def index(request):
+    template = 'posts/index.html'
+    text = "Это главная страница проекта Yatube"
+    context = {
+        'text':text,
+    }
+    return render(request, template, context) 
 def group_posts (request,slug):
-    return HttpResponse('Что-то')
+    template = 'posts/group_list.html'
+    text = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'text':text,
+    }
+    return render(request, template, context)
